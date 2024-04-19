89443a48059b673a21f35f75ea98fe7173f56fb6
PavlovaKate <kv.pavlova@mail.ru>
2024-04-19 14:25:00 +0300

    Edit emoji

--- src/game-models/Enemy.js
+++ src/game-models/Enemy.js
@@ -7,20 +7,7 @@ class Enemy {
   }
 
   generateSkin() {
-    const skins = [
-      '👾',
-      '💀',
-      '👹',
-      '👻',
-      '👽',
-      '👿',
-      '💩',
-      '🤡',
-      '🤺',
-      '🧛',
-      '🧟',
-      '🎃',
-    ];
+    const skins = ['👀', '👃', '👅', '👄', '👂', '👌', '🍑'];
     this.skin = skins[Math.floor(Math.random() * skins.length)];
   }
 
--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -2,7 +2,7 @@
 
 class Hero {
   constructor({ position }) {
-    this.skin = '🤠'; // можете использовать любые emoji '💃'
+    this.skin = '🙋';
     this.position = position;
   }
 
